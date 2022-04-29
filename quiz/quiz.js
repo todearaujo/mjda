dbb868dd8f6ab5824a3c69459dc29a07ca202fd0
Thiago Araújo <github@todearaujo.com>
2022-04-29 20:30:25 -0300

    Align

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,18 +1,20 @@
-let prespostalet = document.querySelector( '.presposta')
+let prespostalet = document.querySelector('.presposta')
 let divrespostalet = document.querySelector('.divresposta')
+let idquiz = document.getElementById('quiz')
         
-function checarResposta( event ) {
+function checarResposta(event) {
     if ( event.target.classList.contains ('certo') ) {
         prespostalet.textContent = "Acertou ✅"
-        console.log ('Acertou')
+        console.log('Acertou')
     }
         else { 
         prespostalet.textContent = "Errou ❌"
-        console.log ('Errou')
+        console.log('Errou')
     }
     
     divrespostalet.style.display = 'block'
-    
+    idquiz.classList.add('inativo')
+    event.target.classList.add('clicado')
 }
 
 let items = document.querySelectorAll('.item')
