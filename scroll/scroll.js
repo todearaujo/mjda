a9c5ba45edf68c0007681704e66e13ca421c381f
Thiago Araújo <github@todearaujo.com>
2022-05-17 23:48:13 -0300

    Scroll v 3

--- scroll/scroll.js
+++ scroll/scroll.js
@@ -7,17 +7,39 @@ function escutaScroll(event){
         let posicao = slide.getBoundingClientRect();
 
         let target = slide.dataset.sn;
-
-        let phone = document.querySelector('.'+target);
+        
+        let phone = document.querySelector('.'+target);     
+        
         let tamanho = document.querySelector('.t'+target);   
 
         if(posicao.top <= 0 && posicao.top > -posicao.height){
             phone.classList.add('show');
             tamanho.classList.add('show');
+
+            let regua = document.querySelector('#phones')
+
+                if(target === ('s00')) {  
+                    regua.classList.add('hideruler')
+                }
+                else if(target === ('s16')) {  
+                    regua.classList.add('hideruler')
+                    document.querySelector('[data-sn="s16"]').style.visibility = "visible";
+                }
+                else if(target === ('s17')) {
+                    regua.classList.add('hideruler')
+                    document.querySelector('[data-sn="s16"]').style.visibility = "hidden";
+                }
+                else{
+                    regua.classList.remove('hideruler')
+                }
+
         }else{
             phone.classList.remove('show');
             tamanho.classList.remove('show');
         }
+
+        
+
     }
 
 }
