af369b94548afadf3969b1832ff4cd5d597652c7
Thiago Araújo <github@todearaujo.com>
2022-06-06 07:08:54 -0300

    Calc v 6.1

--- calc/app.js
+++ calc/app.js
@@ -11,13 +11,13 @@ let geracoes = [
     "nome": 'Perdida',
     "de": 1883,
     "ate": 1900,
-    "texto": '<b>Geração Perdida<br>(1833 - 1900)</b><br><br>Os <i>perdidos</i> passaram pela Primeira Guerra Mundial e viveram os <i>Loucos Anos 20</i>.<br><br>Cunhado pela poetisa americana Gertrude Stein, o nome alude ao sentimento de desorientação que marcou essa geração.'
+    "texto": '<b>Geração Perdida<br>(1833 - 1900)</b><br><br>Os <i>perdidos</i> passaram pela Primeira Guerra Mundial e curtiram muito os <i>Loucos Anos 20</i>.<br><br>Cunhado pela poetisa americana Gertrude Stein, o nome alude ao sentimento de desorientação que marcou essa geração.'
   },
   {
     "nome": 'Grandiosa',
     "de": 1901,
     "ate": 1927,
-    "texto": '<b>Geração Grandiosa<br>(1901 - 1927)</b><br><br>Passaram pela Segunda Guerra Mundial e viveram a <i><b>Grande depressão (Greatest depression)</b></i> e os <i>Trinta Anos Gloriosos</i>.<br><br>Cunhado pelo jornalista americano Tom Brokaw, o nome é uma direta referência ao termo em inglês <i>greatest</i>.'
+    "texto": '<b>Geração Grandiosa<br>(1901 - 1927)</b><br><br>Passaram pela Segunda Guerra Mundial e viveram a Grande depressão <i>(Greatest depression)</i> e os <i>Trinta Anos Gloriosos</i>.<br><br>Cunhado pelo jornalista americano Tom Brokaw, o nome é uma direta referência ao termo em inglês <i>greatest</i>.'
   },
   {
     "nome": 'Silenciosa',
@@ -29,31 +29,31 @@ let geracoes = [
     "nome": 'Baby Boomers',
     "de": 1946,
     "ate": 1964,
-    "texto": 'Baby Boomers'
+    "texto": '<b>Baby Boomers<br>(1946 - 1964)</b><br><br>Nascidos após a segunda-guerra mundial em um <i>boom</i> populacional que marcou a época.<br><br>Enquanto jovens, os <i>boomer</i> participaram dos movimentos de contracultura em alguns países nos anos 60, questionando muitos valores.'
   },
   {
     "nome": 'Geração X',
     "de": 1965,
     "ate": 1980,
-    "texto": 'Geração X'
+    "texto": '<b>Geração X<br>(1965 - 1980)</b><br><br>Nascidos entre meados dos anos 60 até o ano de 1980, a geração é marcada por mudanças nos valores sociais como o aumento no número de divórcios e o "retorno das mães" à força de trabalho.<br><br>Enquanto adolescentes e jovens adultos nos anos 80/90, os "X" também são chamados de "geração MTV", em referência ao canal musical e a invenção e popularização dos videoclipes.'
   },
   {
-    "nome": 'Y Millenials',
+    "nome": 'Y / Millennials',
     "de": 1981,
     "ate": 1996,
-    "texto": 'Millenials'
+    "texto": '<b>Geração Y<br>Millennials<br>(1981 - 1996)</b><br><br>Filhos únicos ou com um só irmão, <i>millenials</i> são da primeira geração encarada como fenômeno planetário, pois são filhos da globalização.<br><br>Pioneiros entre os "nativos digitais", o nome desta geração faz referência ao momento em que se tornaram adolescentes e jovens adultos na virada dos anos 2000.'
   },
   {
-    "nome": 'Z Zoomers',
+    "nome": 'Z / Zoomers',
     "de": 1997,
     "ate": 2012,
-    "texto": 'Zoomers' 
+    "texto": '<b>Geração Z<br>Zoomers<br>(1997 - 2012)</b><br><br>Verdadeiros <i>nativos digitais</i>, o nome é uma referência aos <i>baby boomers</i>, por similaridades entre a geração do passado e os "Z".<br><br>Tem aversão à riscos e são mais <i>comportados</i> que seus antecessores. Esta é a primeira geração que passa mais tempo em telas do que com "papel".' 
   },
   {
     "nome": 'Alpha',
     "de": 2013,
     "ate": 2025,
-    "texto": 'Alpha'
+    "texto": '<b>Geração Alpha<br>(2013 - 2025)</b><br><br>Primeiros a nascer integralmente no século 21, os <i>alpha</i> são em sua maioria filhos da geração <i>millennial</i>.<br><br>Seu nome ainda está em disputa pelos estudiosos, pois alguns acreditam que a covid-19 é o marco fundamental da geração, migrando então para a letra "C".'
   }
 ]
 
@@ -116,7 +116,7 @@ function calcular( anonasc ) {
       //Definir numero como total de gerações menos o índice atual - 1,
       //pois os blocos começam a contagem no 0
       let voce = 100 - ( ( geracao.ate - anonasc ) * 100 / ( geracao.ate - geracao.de ) )
-      destacarBloco( numerobloco, nomegeracao, texto )
+      destacarBloco( numerobloco, texto )
       posicaoMarcador( numerobloco, anonasc, voce )
       break
     }
